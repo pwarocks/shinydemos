77ed13392198ae04e6ca0ca4beb0688aa0910a12
Mike Taylor <miketaylr@gmail.com>
2011-09-23 15:58:17 -0400

    most basic conversion of my demo to one that matches yennys mocks

---
+++ getusermedia_photobooth_app/js/app.js
@@ -0,0 +1,92 @@
+(function(win, doc){
+  var video = doc.querySelector('video'),
+      snapshots = doc.getElementById('pics'),
+      photos = snapshots.getContext('2d'),
+      img = new Image(),
+      audio = new Audio('media/click.ogg'),
+      button = doc.querySelector('button'),
+      VIDEO_WIDTH, VIDEO_HEIGHT,
+      snaps = [
+        function(){photos.drawImage(video, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT, 5, 5, 150, 112.5);},
+        function(){photos.drawImage(video, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT, 5, 124, 150, 112.5);},
+        function(){photos.drawImage(video, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT, 5, 244, 150, 112.5);},
+        function(){photos.drawImage(video, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT, 5, 363, 150, 112.5);}
+      ];
+      
+  var canvasPrep = (function(){
+    photos.fillStyle = '#fff';
+    photos.fillRect(0,0,160,480);
+    photos.lineWidth = 2;
+    photos.strokeStyle = '#515151';
+    photos.strokeRect(0,0,160,480)
+  }());
+
+  var computeAspect = function(){
+    //compute correct size/aspect ratio for picture frame
+    //browsers that don't have object fit will look wacky
+    VIDEO_WIDTH = video.videoWidth;
+    VIDEO_HEIGHT = video.videoHeight;
+  };
+
+  var takeSnaps = function(interval){
+    var i = 0,
+    id = setInterval(function(){
+      if (snaps.length){
+        snaps.shift()();
+        audio.play();
+      }
+    
+      if (++i == 4){
+        clearInterval(id);
+        clickToSend();
+      }
+    }, interval);
+  };
+
+  var fallback = function(){
+    video.onloadedmetadata = function(){
+      //not all browsers have video@muted yet.
+      this.muted = true;
+      computeAspect();
+    };
+  };
+
+  var clickToSend = function(){
+    snapshots.onclick = function(){
+      //do this because super long data URIs cause opera to screech to a halt
+      sessionStorage.setItem('snap', this.toDataURL());
+      location.href='snap.html';
+    };
+  };
+
+  //http://goo.gl/zTZY0 thx @unscriptable
+  var debounce = function(func, threshold, execAsap) {
+    var timeout;
+    return function debounced() {
+      var obj = this, args = arguments;
+      function delayed() {
+        if (!execAsap) func.apply(obj, args);
+        timeout = null; 
+      };
+      if (timeout) clearTimeout(timeout);
+      else if (execAsap) func.apply(obj, args);
+      timeout = setTimeout(delayed, threshold || 100); 
+    };
+  };
+  
+  var init = (function(){
+    
+    navigator.getUserMedia ? 
+      navigator.getUserMedia('video', function(stream){
+        video.src = stream;
+        video.onloadedmetadata = function(){
+          computeAspect();
+        }; 
+      }, fallback) : fallback();
+  }());
+  
+  button.onclick = function(){
+    console.log('hi');
+    takeSnaps(1200);
+  };
+}(window, document));

d2a6a09d3def1f808a49fc7bcab9e9004a19a39a
Divya Manian <divya.manian@gmail.com>
2011-12-28 15:39:59 -0800

    yay index.html now renders based on data in config.yaml

--- compiler/index.js
+++ compiler/index.js
@@ -5,28 +5,56 @@ var yaml = require('js-yaml');
 
 var configs = require('../config.yaml').shift();
 
+Handlebars.registerHelper('hyphenatedTag', function(tag) {
+  return tag.split(' ').join('-');
+});
+
 var paths = {
   deploy: './deploy',
   source: './source',
   demos: './demos',
-  layouts: './layouts' 
+  layouts: {
+    home: './layouts/index.html',
+    demo: './layouts/demo.html',
+    tag: './layouts/tag.html' 
+  }
 }
+
+var homepageTemplate = Handlebars.compile(fs.readFileSync(paths.layouts.home).toString());
+  
+var tags = [], currenttag;
 
 
 // Copy all demos to deployment folder
+
+/*
 ncp(paths.demos, paths.deploy + '/' + paths.demos, function(err) {
   if(err)
-    console.error(err);
-
-  // Copy Assets and Styles to deployment folder
-  ncp(paths.source, paths.deploy, function(err) {
-    if(err)
-      console.error("error copying source", err)
-  }); 
-  
-  
-  console.log('done!');
+    console.error(err);  
+console.log('copying demos to %s', paths.deploy + '/' + paths.demos);
 }); 
+
+// Copy Assets and Styles to deployment folder
+ncp(paths.source, paths.deploy, function(err) {
+  if(err)
+    console.error("error copying source", err)
+console.log('copying source assets to %s from %s', paths.deploy, paths.source);
+}); 
+
+*/
+//Render index.html with our configs
+for(var i = 0; i < configs.length; i++) {
+  [].forEach.apply(configs[i].tags, [function(tag, i) {
+    if(tags.indexOf(tag) == -1) {
+      tags.push(tag);
+    }
+  }]);
+}
+
+var homepageRender = homepageTemplate({tags: tags});
+
+fs.writeFileSync(paths.deploy + '/index.html', homepageRender);
+
 
 
 

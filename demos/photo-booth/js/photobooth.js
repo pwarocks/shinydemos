78a50352af841c6cf6732fa752f63c8879f1102a
Mike Taylor <miket@opera.com>
2012-01-05 10:53:25 -0600

    fix paths for photo-booth resources

--- demos/photo-booth/js/photobooth.js
+++ demos/photo-booth/js/photobooth.js
@@ -13,7 +13,7 @@
       shareimg = new Image(),
       corner = new Image(),
       canvasBg = new Image(),
-      audio = new Audio('media/click.ogg'),
+      audio = new Audio('http://media.shinydemos.com/photo-booth/click.ogg'),
       button = doc.querySelector('canvas + button'),
       container = doc.getElementById('container'),
       spinner = doc.getElementById('spinner'),
@@ -28,12 +28,12 @@
       ];
       
   var canvasPrep = (function(){
-    canvasBg.src = '../../source/images/photobooth/bg_output.png';
+    canvasBg.src = 'images/bg_output.png';
     canvasBg.onload = function(){
      photos.drawImage(canvasBg, 0, 0); 
     }
-    corner.src = '../../source/images/photobooth/corner.png';
-    snap.src = '../../source/images/photobooth/img_border.png';
+    corner.src = 'images/corner.png';
+    snap.src = 'images/img_border.png';
     snap.onload = function(){
       photos.drawImage(snap, 16, 23);
       photos.drawImage(snap, 181, 23);
@@ -102,14 +102,14 @@
   };
   
   var startButton = function(){
-    countdown.src = '../../source/images/photobooth/countdown.svg';
+    countdown.src = 'images/countdown.svg';
     countdown.id = 'countdown';
     container.appendChild(countdown);
     setTimeout(takeSnaps, 4000, 1200);
   };
   
   var share = function(){
-    shareimg.src = '../../source/images/photobooth/img_border_hover.png';
+    shareimg.src = '../images/img_border_hover.png';
     shareimg.onload = function(){
       photos.drawImage(this, 512, 23);
       photos.drawImage(corner, 635, 23);
@@ -129,7 +129,7 @@
   };
   
   var repaint = function(){
-    shareimg.src = '../images/photobooth/img_repaint.png';
+    shareimg.src = '../images/img_repaint.png';
     shareimg.onload = function(){
       photos.drawImage(this, 512, 23);
     };

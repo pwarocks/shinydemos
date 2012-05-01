14863a1d6dde06229b155cf1aa087d0b2b8d9802
Mike Taylor <miket@opera.com>
2012-05-01 14:26:34 -0500

    add two deploy environments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,40 @@
-# replace these with your server's information
 set :domain,  "shinydemos.com"
 set :user,    "shinydemos"
 
 # name this the same thing as the directory on your server
-set :application, "dev.shinydemos.com"
-
-# or use a hosted repository
-#set :repository, "ssh://user@example.com/~/git/test.git"
+set :application, "shinydemos.com"
 
 default_run_options[:pty] = true  # Must be set for the password prompt from git to work
-set :repository, "git@github.com:operasoftware/shinydemos.git"  # Your clone URL
+set :repository, "git@github.com:operasoftware/shinydemos.git"
 set :scm, "git"
-set :user, "shinydemos"  # The server's user for deploys
-# set :scm_passphrase, "p@ssw0rd"  # The deploy user's password
 ssh_options[:forward_agent] = true
 
-server "#{domain}", :app, :web, :db, :primary => true
+server "#{domain}", :app, :web, :primary => true
 
 set :deploy_via, :remote_cache
-set :copy_exclude, [".git", ".DS_Store"]
+set :copy_exclude, [".git"]
 
-set :branch, "master"
 # set this path to be correct on yoru server
-set :deploy_to, "/home/#{user}/#{application}"
 set :use_sudo, false
 set :keep_releases, 3
 
 # ssh_options[:paranoid] = false
+
+#cap production deploy
+task :production do
+  set :branch, "master"
+  role :web, "shinydemos.com"
+  set :user, "shinydemos"
+  set :deploy_to, "/home/shinydemos/#{application}"
+end
+
+#cap dev deploy
+task :dev do
+  set :branch, "deploy"
+  role :web, "homes.oslo.osa"
+  set :user, "devrelbot"
+  set :deploy_to, "/home/devrelbot/public_html/dev.#{application}"
+end
 
 # this tells capistrano what to do when you deploy
 namespace :deploy do

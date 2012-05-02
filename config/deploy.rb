542ae1b082247122f630d4761898a2141cd657cf
Mike Taylor <miket@opera.com>
2012-05-02 13:02:30 -0500

    change tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-set :domain,  "shinydemos.com"
-set :user,    "shinydemos"
-
 # name this the same thing as the directory on your server
 set :application, "shinydemos.com"
 
@@ -9,8 +6,6 @@ set :repository, "git@github.com:operasoftware/shinydemos.git"
 set :scm, "git"
 ssh_options[:forward_agent] = true
 
-server "#{domain}", :app, :web, :primary => true
-
 set :deploy_via, :remote_cache
 set :copy_exclude, [".git"]
 
@@ -18,22 +13,20 @@ set :copy_exclude, [".git"]
 set :use_sudo, false
 set :keep_releases, 3
 
-# ssh_options[:paranoid] = false
-
 #cap production deploy
 task :production do
-  set :branch, "master"
-  role :web, "shinydemos.com"
-  set :user, "shinydemos"
-  set :deploy_to, "/home/shinydemos/#{application}"
+  set  :branch,     "master"
+  role :web,        "shinydemos.com"
+  set  :user,       "shinydemos"
+  set  :deploy_to,  "/home/shinydemos/#{application}"
 end
 
 #cap dev deploy
 task :dev do
-  set :branch, "deploy"
-  role :web, "homes.oslo.osa"
-  set :user, "devrelbot"
-  set :deploy_to, "/home/devrelbot/public_html/dev.#{application}"
+  set  :branch,     "deploy"
+  role :web,        "homes.oslo.osa"
+  set  :user,       "devrelbot"
+  set  :deploy_to,  "/home/devrelbot/public_html/dev.#{application}"
 end
 
 # this tells capistrano what to do when you deploy

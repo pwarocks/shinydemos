4e3fe252e9942e28f2d600a1f48d08a6afffb2a8
Mike Taylor <miket@opera.com>
2012-06-04 16:42:33 -0500

    Some tests for lib/categories

--- test/categories-test.js
+++ test/categories-test.js
@@ -1,1 +1,52 @@
-//put tests here!
+var vows = require('vows'),
+	assert = require('assert'),
+	yaml = require('js-yaml'),
+	config = require('../config.yaml'),
+	category = require('../lib/categories');
+	
+//TODO: consider fixtures in case config.yml copy changes
+
+vows.describe('Category filtering').addBatch({
+	'When getting a displayName from': {
+		'CSS3': {
+			topic: category.getPropFromTag('displayName', 'css3'),
+			'css3 -> "CSS 3"': function(topic){
+				assert.equal(topic, 'CSS 3');
+			}	
+		},
+		'SVG': {
+			topic: category.getPropFromTag('displayName', 'svg'),
+			'svg -> "SVG"': function(topic){
+				assert.equal(topic, 'SVG');
+			}
+		}
+	},
+	'When getting a tagline from': {
+		'CSS3': {
+			topic: category.getPropFromTag('tagline', 'css3'),
+			'contains Håkon Wium Lie': function(topic){
+				assert.match(topic, /Håkon Wium Lie/);
+			}
+		},
+		'SVG': {
+			topic: category.getPropFromTag('tagline', 'svg'),
+			'contains Scalable Vector Graphics': function(topic){
+				assert.match(topic, /Scalable Vector Graphics/);
+			}
+		}
+	},
+	'When getting a non-existent property': {
+		topic: 'IDontExist',
+		'it should throw': function(topic){
+			assert.throws(function(){
+				category.getPropFromTag(topic, 'css3')
+			}, Error);
+		}
+	},
+	'When passing a bogus tag': {
+		topic: category.getPropFromTag('displayName', 'ponies'),
+		'it should return undefined': function(topic){
+			assert.isUndefined(topic);
+		}
+	}
+}).export(module);

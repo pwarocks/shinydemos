a727a9c36950e619f8fb6a8f7ce9714913e5ac8c
Mike Taylor <miket@opera.com>
2012-06-04 22:18:18 -0500

    Now we can run tests on all values of config.yml, or just a few (node test/foo.js vs. vows)

--- test/config-features-test.js
+++ test/config-features-test.js
@@ -9,46 +9,51 @@ var vows = require('vows'),
 //   title: 'App Cache'
 //   link: 'http://www.whatwg.org/specs/web-apps/current-work/multipage/offline.html'
 
-vows.describe('Demo feature properties').addBatch({
-	'A slug': {
-		topic: features[0].slug,
-		'should be lowercase (numbers OK)': function(topic){
-			assert.match(topic, /^[a-z0-9]+$/);
-		},
-		'should not be undefined': function(topic){
-			assert.isDefined(topic);	
-		},
-		'should not contain spaces': function(topic){
-			assert.match(topic, /^[^\s]+$/)
+// if you run tests via vows, this only happens once
+// to have it check every single feature in config.yml
+// node config-features-test.js seems to do the trick.
+for (var i = 0, feature; feature = features[i]; i++) {
+	vows.describe('Demo feature properties').addBatch({
+		'A slug': {
+			topic: feature.slug,
+			'should be lowercase (numbers OK)': function(topic){
+				assert.match(topic, /^[a-z0-9]+$/);
+			},
+			'should not be undefined': function(topic){
+				assert.isDefined(topic);	
+			},
+			'should not contain spaces': function(topic){
+				assert.match(topic, /^[^\s]+$/)
+			}
 		}
-	}
-}).addBatch({
-	'A title': {
-		topic: features[0].title,
-		'should be a string': function(topic){
-			assert.isString(topic);
-		},
-		'should not be empty': function(topic){
-			assert.isNotEmpty(topic);
-		},
-		'should not be undefined': function(topic){
-			assert.isDefined(topic);	
+	}).addBatch({
+		'A title': {
+			topic: feature.title,
+			'should be a string': function(topic){
+				assert.isString(topic);
+			},
+			'should not be empty': function(topic){
+				assert.isNotEmpty(topic);
+			},
+			'should not be undefined': function(topic){
+				assert.isDefined(topic);	
+			}
 		}
-	}
-}).addBatch({
-	'A link': {
-		topic: features[0].link,
-		'should be a string': function(topic){
-			assert.isString(topic);
-		},
-		'should not be empty': function(topic){
-			assert.isNotEmpty(topic);
-		},
-		'should not be undefined': function(topic){
-			assert.isDefined(topic);	
-		},
-		'should smell like a hyperlink': function(topic){
-			assert.match(topic, /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/i);
+	}).addBatch({
+		'A link': {
+			topic: feature.link,
+			'should be a string': function(topic){
+				assert.isString(topic);
+			},
+			'should not be empty': function(topic){
+				assert.isNotEmpty(topic);
+			},
+			'should not be undefined': function(topic){
+				assert.isDefined(topic);	
+			},
+			'should smell like a hyperlink': function(topic){
+				assert.match(topic, /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/i);
+			}
 		}
-	}
-}).export(module);
+	}).export(module);
+}

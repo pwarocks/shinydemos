12a7e26b089b2f22d3a92cf0282a4c45854c90a0
Mike Taylor <miket@opera.com>
2012-06-25 23:38:59 -0500

    remove unused globals objects

--- grunt.js
+++ grunt.js
@@ -22,19 +22,16 @@ module.exports = function(grunt) {
 			// Just for the lint:grunt target.
 			grunt: {
 				options: {node: true},
-				globals: {}
 			},
 			
 			// Just for the lint:tests target.
 			tests: {
 				options: {node: true, loopfunc: true, boss: true},
-				globals: {}
 			},
 			
 			// Just for the lint:tests target.
 			lib: {
 				options: {browser: true, boss: true},
-				globasl: {}
 			},
 			
 			shared: {
